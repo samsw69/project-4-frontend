e3a934660131c0b745afc9bb864459ae882cfa30
Samsw69 <sam@housenhome.co.uk>
2017-05-11 21:24:15 +0100

    updates to resolve github auth

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
 angular
   .module('artsy', ['ui.router', 'ngResource', 'satellizer', 'ngMessages', 'checklist-model', 'ui.bootstrap', 'ngAnimate'])
-  .constant('API_URL', 'http://localhost:3000/api');
+  // .constant('API_URL', 'http://localhost:3000/api');
+  .constant('API_URL', 'http://evening-beyond-20459.herokuapp.com');
